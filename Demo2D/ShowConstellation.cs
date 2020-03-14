3196316fd95cc1c52a51065e39d8bbb64985b7bf
laheller <ladislav.heller@gmail.com>
2020-03-14 16:43:23 +0100

    Not fully working yet ShowConstellation feature in Demo2D

--- Demo2D/ShowConstellation.cs
+++ Demo2D/ShowConstellation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
@@ -12,9 +13,11 @@ namespace Demo2D {
             InitializeComponent();
 
             const float SF = 4.0f;      // scale factor
+            const int M = 10;
 
             // get constellation boundary from contructor parameter "key"
             var _const = NorthConstellations.ContainsKey(key) ? NorthConstellations[key] : SouthConstellations[key];
+            var _cp = Boundaries[key].Centroid;
             var boundary = _const.Boundary;
             var centroid = _const.Centroid;
             var newconst = new PointF[boundary.Length];
@@ -27,6 +30,14 @@ namespace Demo2D {
             var f1 = new Font(FontFamily.GenericMonospace, 2.0f, FontStyle.Regular);
             var sf = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
 
+            var StartPoint = new PointF(-centroid.X, -centroid.Y);
+            var GreatCirclePoints = new List<PointF>();
+            for (var i = 0; i < 360; i += 15) {
+                var px = 180 * (float)Math.Cos(Deg2Rad(i)) - centroid.X;
+                var py = 180 * (float)Math.Sin(Deg2Rad(i)) - centroid.Y;
+                GreatCirclePoints.Add(new PointF(px, py));
+            }
+
             spPanels.Panel1.Paint += (s0, e0) => {
                 var cr = e0.ClipRectangle;
                 var ox = cr.Width / 2;
@@ -38,15 +49,47 @@ namespace Demo2D {
                 gr.TranslateTransform(ox, oy);
                 gr.ScaleTransform(SF, SF);
 
+                var rect = new RectangleF((M - ox) / SF, (M - oy) / SF, 2 * (ox - M) / SF, 2 * (oy - M) / SF);
+                gr.DrawRectangle(black1, rect.X, rect.Y, rect.Width, rect.Height);
                 gr.DrawString(key, f1, Brushes.Black, 0, 0, sf);
 
                 // draw the boundary and grid only after rotation
                 gr.RotateTransform(tbRotation.Value);
 
                 // rectangle below will show the bounds of visible area
-                gr.DrawRectangle(Pens.Green, -ox / SF, -oy / SF, 2 * ox / SF, 2 * oy / SF);
+                //gr.DrawRectangle(Pens.Green, -ox / SF, -oy / SF, 2 * ox / SF, 2 * oy / SF);
 
-                for (var i = 0; i < 360; i += 15) gr.DrawLine(grid, -centroid.X, -centroid.Y, 180 * (float)Math.Cos(Deg2Rad(i)) - centroid.X, 180 * (float)Math.Sin(Deg2Rad(i)) - centroid.Y);
+                for (var i = 0; i < GreatCirclePoints.Count; i++) {
+                    //gr.DrawLine(grid, -centroid.X, -centroid.Y, 180 * (float)Math.Cos(Deg2Rad(i)) - centroid.X, 180 * (float)Math.Sin(Deg2Rad(i)) - centroid.Y);
+                    var EndPoint = GreatCirclePoints[i];
+                    gr.DrawLine(grid, StartPoint, EndPoint);
+
+                    /*
+                    var j = _cp.Y > 0.0f ? i : 24 - i;
+
+                    // calculate coordinates of numbers at left and right side of rectangle
+                    var xp = 0.0f;
+                    if (EndPoint.X < 0) xp = rect.Left;
+                    else if (EndPoint.X > 0) xp = rect.Right;
+                    var y1 = GetY(StartPoint, EndPoint, xp);
+                    var py1 = new PointF(xp, y1);
+                    if (rect.Contains(py1)) {
+                        gr.DrawString(j.ToString(), f1, Brushes.Black, py1, sf);
+                        gr.DrawString((j >= 12 ? j - 12 : j + 12).ToString(), f1, Brushes.Black, new PointF(rect.Right, -y1), sf);
+                    }
+
+                    // calculate coordinates of numbers at to and bottom side of rectange
+                    var yp = 0.0f;
+                    if (EndPoint.Y < 0) yp = rect.Top;
+                    else if (EndPoint.Y > 0) yp = rect.Bottom;
+                    var x1 = GetX(StartPoint, EndPoint, yp);
+                    var px1 = new PointF(x1, yp);
+                    if (rect.Contains(px1)) {
+                        gr.DrawString(j.ToString(), f1, Brushes.Black, px1, sf);
+                        gr.DrawString((j >= 12 ? j - 12 : j + 12).ToString(), f1, Brushes.Black, new PointF(-x1, rect.Bottom), sf);
+                    }
+                    */
+                }
                 for (var i = 10; i <= 150; i += 10) gr.DrawArc(grid, -i - centroid.X, -i - centroid.Y, 2 * i, 2 * i, 0, 360);
                 gr.DrawPolygon(black1, newconst);
 
@@ -66,6 +109,26 @@ namespace Demo2D {
                     MessageBox.Show(ex.Message, "Error");
                 }
             };
+        }
+
+        // equation of line: y = mx + b
+        float GetY(PointF A, PointF B, float X) {
+            var dx = B.X - A.X;
+            if (dx == 0.0f) return float.NaN;
+            var dy = B.Y - A.Y;
+            var m = dy / dx;
+            var b = A.Y - m * A.X;
+            return m * X + b;
+        }
+
+        // equation of line: x = my + b
+        float GetX(PointF A, PointF B, float Y) {
+            var dy = B.Y - A.Y;
+            if (dy == 0.0f) return float.NaN;
+            var dx = B.X - A.X;
+            var m = dx / dy;
+            var b = A.X - m * A.Y;
+            return m * Y + b;
         }
 
         protected override CreateParams CreateParams {
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # IAU.Constellations
 A .NET library to work with IAU constellations
+
+Build
+=====
+- Install Visual Studio 2017 Community Edition or newer
+- Open solution file: Constellations.sln
+- Build
+
+Test
+====
+- Run Demo2D application to explore library feautres
